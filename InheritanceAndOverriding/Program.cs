387f4a379e7bed76d2ad4329f0cfe3addc6e013d
Timothyninson <tkninson@st.ug.edu.gh>
2025-07-30 15:52:13 +0000

    Added Inheritance and Overriding program

---
+++ InheritanceAndOverriding/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace InheritanceAndOverriding
+{
+    class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal a = new Animal();
+            Animal d = new Dog();
+            Animal c = new Cat();
+
+            a.MakeSound();
+            d.MakeSound();
+            c.MakeSound();
+        }
+    }
+}
